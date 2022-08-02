dbce0d6773123272be0029ce0f0860aa7e9b1095
jz <jiangzhangsg@qq.com>
2022-08-02 21:50:14 +0800

    modify string hash

--- src/interface.cpp
+++ src/interface.cpp
@@ -29,3 +29,7 @@ size_t engine_read(void *ctx, int32_t select_column,
   Engine *engine = (Engine *)ctx; 
   return engine->read(select_column, where_column, column_key, column_key_len, res);
 }
+
+bool operator==(const UserString &l, const UserString &r) {
+    return std::string(l.ptr, 128) == std::string(r.ptr, 128);
+}
