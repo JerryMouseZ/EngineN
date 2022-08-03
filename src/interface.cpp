497b3376fe5dba113d3556933618253c2172ba09
jz <jiangzhangsg@qq.com>
2022-08-03 17:53:01 +0800

    time

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,18 +1,27 @@
 #include "../inc/interface.h"
+#include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <string.h>
+#include "include/data.hpp"
 #include "include/engine.hpp"
+#include <sys/time.h>
 
+timeval begin, end;
 
 void* engine_init(const char* host_info, const char* const* peer_host_info, size_t peer_host_info_num,
                   const char* aep_dir, const char* disk_dir) {
+  gettimeofday(&begin, NULL);
   Engine *engine = new Engine();
   engine->open(aep_dir, disk_dir);
+  gettimeofday(&end, NULL);
+  DEBUG_PRINTF(0, "init time %lf s\n", (end.tv_usec - begin.tv_usec) / (double) (1000000) + end.tv_sec - begin.tv_sec);
   return engine;
 }
 
 void engine_deinit(void *ctx) {
+  gettimeofday(&begin, NULL);
+  DEBUG_PRINTF(0, "operating time %lf s\n", (begin.tv_usec - end.tv_usec) / (double) (1000000) + begin.tv_sec - end.tv_sec);
   if (ctx)
     delete reinterpret_cast<Engine*>(ctx);
 }
