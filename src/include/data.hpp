0162867c3f603334459d14a669359fa8c866d660
jz <jiangzhangsg@qq.com>
2022-08-18 08:54:33 +0800

    add more buckets; remove prefetch

--- src/include/data.hpp
+++ src/include/data.hpp
@@ -172,7 +172,7 @@ public:
     }
     
     // prefetch write
-    __builtin_prefetch(ptr + write_offset, 1, 0);
+    /* __builtin_prefetch(ptr + write_offset, 1, 0); */
     // 可以留到flag一起drain
     pmem_memcpy_persist(ptr + write_offset, &user, sizeof(User));
     return write_offset;
