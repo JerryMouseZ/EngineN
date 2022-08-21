7ec65c3b568593d6ebeddc22cd794e8ced47da55
jz <jiangzhangsg@qq.com>
2022-08-21 11:14:35 +0800

    add inline keys

--- test/test_engine_read.cpp
+++ test/test_engine_read.cpp
@@ -4,9 +4,9 @@
 enum Column{Id=0, Userid, Name, Salary};
 void test_engine_read_salary(int i)
 {
-  void *context = engine_init(nullptr, nullptr, 0, "/tmp/aep/", "/tmp/disk/");
-  long salary = i / 2000;
-  int64_t ids[2000] = {0};
+  void *context = engine_init(nullptr, nullptr, 0, "/mnt/aep/", "/mnt/disk/");
+  long salary = i / 4;
+  int64_t ids[4] = {0};
   int ret = engine_read(context, Id, Salary, &salary, sizeof(salary), ids);
   fprintf(stderr, "%d\n", ret);
   engine_deinit(context);
