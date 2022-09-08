daccae6201ec288b18c7749bf038aab9b86dfc7b
JerryMouseZ <1711333@mail.nankai.edu.cn>
2022-09-08 09:31:40 +0800

    test liburing

---
+++ src/include/communicator.hpp
@@ -0,0 +1,14 @@
+#ifndef COMM_H
+#define COMM_H
+#include <netinet/in.h>
+#include <liburing.h>
+
+class Communicator{
+public:
+  Communicator(char *ip, int port, char **peers) {
+    
+  }
+};
+
+#endif
+
