d061b800e4f64359e43a93d3694762ff1b543ce6
JerryMouseZ <1711333@mail.nankai.edu.cn>
2022-10-09 08:23:51 +0800

    fix local bug

--- src/engine.cpp
+++ src/engine.cpp
@@ -14,7 +14,7 @@
 #include <string>
 #include <thread>
 
-Engine::Engine(): datas(nullptr), id_r(nullptr), uid_r(nullptr), sala_r(nullptr), consumers(nullptr) {
+Engine::Engine(): datas(nullptr), id_r(nullptr), uid_r(nullptr), sala_r(nullptr), consumers(nullptr), alive{false}, neighbor_index{0} {
   host_index = -1;
   qs = static_cast<UserQueue *>(mmap(0, MAX_NR_CONSUMER * sizeof(UserQueue), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
   for (int i = 0; i < MAX_NR_CONSUMER; i++) {
