128cda58cb41a5b0f3f4c7bc600d9a4a23b7f0c5
jz <jiangzhangsg@qq.com>
2022-10-12 23:50:36 +0800

    need to change sync_queue to min_head

--- test/test_sync_queue.cpp
+++ test/test_sync_queue.cpp
@@ -32,7 +32,7 @@ void test_engine(void *context, int index, size_t num)
   long per_thread = num / 50;
   std::thread *threads[50];
   for (long tid = 0; tid < 50; ++tid) {
-    /* threads[tid] = new std::thread([=]{ */
+    threads[tid] = new std::thread([=]{
     long data_begin = index * num + tid * per_thread, data_end = index * num + (tid + 1) * per_thread;
     for (long i = data_begin; i < data_end; ++i) {
       TestUser user;
@@ -45,11 +45,8 @@ void test_engine(void *context, int index, size_t num)
       user.salary = i % num;
       engine_write(context, &user, sizeof(user));
     }
-  }  
-  sleep(10);
+    sleep(10);
 
-  for (long tid = 0; tid < 50; ++tid) {
-    long data_begin = index * num + tid * per_thread, data_end = index * num + (tid + 1) * per_thread;
     for (long i = data_begin; i < data_end; ++i) {
       TestUser user;
       // Select Salary from ... where Id
@@ -73,13 +70,13 @@ void test_engine(void *context, int index, size_t num)
       }
       assert(ret == 4);
     }
-    /* }); */
+    });
   }
 
-  /* for (int tid = 0; tid < 50; tid++) { */
-  /*   threads[tid]->join(); */
-  /*   delete threads[tid]; */
-  /* } */
+  for (int tid = 0; tid < 50; tid++) {
+    threads[tid]->join();
+    delete threads[tid];
+  }
 }
 
 
