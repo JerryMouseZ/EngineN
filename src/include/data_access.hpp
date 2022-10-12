3f40cd9c63b5fc46bd84d83b7e8be22d1cb01991
jz <jiangzhangsg@qq.com>
2022-10-12 13:37:26 +0800

    add retry read from write buffer

--- src/include/data_access.hpp
+++ src/include/data_access.hpp
@@ -3,6 +3,8 @@
 #include "data.hpp"
 #include "queue.hpp"
 #include <cassert>
+#include <cstddef>
+#include <cstdint>
 
 using UserQueue = LocklessQueue<QCMT_ALIGN>;
 
@@ -16,43 +18,49 @@ public:
   DataAccess(Data *datas, UserQueue *qs)
     : datas(datas), qs(qs) {}
 
-  const User *read(uint32_t encoded_index) {
+  const User *read(uint32_t encoded_index, size_t &uncommited_index) {
     uint32_t qid = encoded_index >> 28;
     uint32_t index = encoded_index & ((1 << 28) - 1);
     auto data = &datas[qid];
-    
+    uncommited_index = UINT64_MAX;
     if (qs == nullptr)
       return data->data_read(index);
 
-    // for local
     auto q = &qs[qid];
-    // for remote
-
     if (likely(index < q->min_uncommitted_data_index())) {
       return data->data_read(index);
     }
 
+    /* DEBUG_PRINTF(WBREAD, "Try to read from write buffer: qid = %u, index = %u, but blocked\n", qid, index); */
+
+    /* int yield_cnt = 0; */
+    /* while (index >= q->min_uncommitted_data_index() && yield_cnt < 50) { */
+    /*   sched_yield(); */
+    /*   yield_cnt++; */
+    /* } */
+
+    /* DEBUG_PRINTF(WBREAD, "Block released from write buffer: qid = %u, index = %u\n", qid, index); */
     q->pop_at(&race_data, index);
-
     std::atomic_thread_fence(std::memory_order_release);
-
-    DEBUG_PRINTF(WBREAD, "Try to read from write buffer: qid = %u, index = %u, but blocked\n", qid, index);
-
-    int yield_cnt = 0;
-    while (index >= q->min_uncommitted_data_index() && yield_cnt < 50) {
-      sched_yield();
-      yield_cnt++;
-    }
-
-    DEBUG_PRINTF(WBREAD, "Block released from write buffer: qid = %u, index = %u\n", qid, index);
-
-    if (unlikely(index >= q->min_uncommitted_data_index())) {
-      DEBUG_PRINTF(WBREAD, "Read from write buffer: qid = %u, index = %u\n", qid, index);
+    uncommited_index = q->min_uncommitted_data_index();
+    if (index >= uncommited_index) {
+      DEBUG_PRINTF(QDEBUG, "Read from write buffer: qid = %u, index = %u\n", qid, index);
       return &race_data;
     }
 
-    return data->data_read(index);      
+    return data->data_read(index);
   }
+
+  bool need_to_retry(uint32_t encoded_index, size_t uncommited_index) {
+    uint32_t qid = encoded_index >> 28;
+    uint32_t index = encoded_index & ((1 << 28) - 1);
+    auto data = &datas[qid];
+    auto q = &qs[qid];
+    if (qs == nullptr)
+      return false;
+    return index >= uncommited_index && index < q->min_uncommitted_data_index();
+  }
+
 
 private:
   Data *datas;
